15c249b20aa9dc9ccc6e88598af7a6162e5f1c6d
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-01-21 20:08:15 -0300

    database: use timestamp with timezone so we can use DateTime<Utc> in the Rust code

--- migrations/20230111024010_create_users_table.sql
+++ migrations/20230111024010_create_users_table.sql
@@ -1,12 +1,20 @@
 -- Add migration script here
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id uuid PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     accepted_terms_at TIMESTAMP NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at  TIMESTAMP NOT NULL,
-    banned_at TIMESTAMP,
-    deleted_at TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    banned_at TIMESTAMP WITH TIME ZONE ,
+    deleted_at TIMESTAMP WITH TIME ZONE 
+);
+
+CREATE TABLE IF NOT EXISTS user_tags (
+    id uuid PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    user_id uuid NOT NULL,
+    CONSTRAINT fk_user_id
+    FOREIGN KEY(user_id) REFERENCES users(id)
+    ON DELETE NO ACTION
 );
--- migrations/20230118040324_create_timeline_table.sql
+++
@@ -1,9 +0,0 @@
--- Add migration script here
-CREATE TABLE timeline (
-    id uuid PRIMARY KEY,
-    content_creator_username VARCHAR(255) NOT NULL,
-    content_creator_avatar_url VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    media_url VARCHAR(255) NOT NULL,
-    likes INT NOT NULL
-);
